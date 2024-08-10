8457c442b4d1e4f97598e1e23e99867f977e47a6
Decha <dechasaejara@gmail.com>
2024-08-10 11:51:18 +0300

    update Project

--- app/api/product/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export const GET = async () => {
-    const api = process.env.NASA_API_KEY;
-    const data = await fetch("https://newsapi.org/v2/everything?q=ton-coin&language=en&apiKey=17afe42d180f45b6ba1c2f5e58e49057", { next: { revalidate: 500 } });
-    const jsonData = await data.json();
-    return NextResponse.json(jsonData, { status: 200 })
-
-}
